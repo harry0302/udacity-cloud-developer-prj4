660ea8625d8efa72a31def8ddd59dd1d824fea17
unknown <nguyenngochadt99@gmail.com>
2022-07-19 22:37:52 +0700

    first commit

---
+++ client/src/config.ts
@@ -0,0 +1,9 @@
+// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
+const apiId = 'd753c90c0h'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-uqpv236a.us.auth0.com',            // Auth0 domain
+  clientId: 'LiiM3Zdn7Wyu0UfpK969Cyfs71ik1xsm',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
